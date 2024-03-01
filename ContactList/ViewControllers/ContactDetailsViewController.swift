a7be482e2c0cbdfbbe53ed44e2cc17e30b3418b0
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-01 14:00:15 +0100

    Show contacts in ContactListViewController

--- ContactList/ViewControllers/ContactDetailsViewController.swift
+++ ContactList/ViewControllers/ContactDetailsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ContactDetailsViewController: UIViewController {
 
+    var persons: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -16,4 +16,26 @@ class ContactListViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        persons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let person = persons[indexPath.row]
+        
+        content.text = person.fuulname
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        
+        let detailcVC = segue.destination as? ContactDetailsViewController
+        detailcVC?.persons = persons[indexPath.row]
+    }
 }
